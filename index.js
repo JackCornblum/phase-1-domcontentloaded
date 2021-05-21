696dd6034e8e1ed51d1e3909f332f4bde6972386
jackcornblum <jcornblum@gmail.com>
2021-05-21 10:07:59 -0400

    commit tree b1add2dd514bc4bdac2385c5788aa562e27b61eb

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const input = document.getElementById("text");
+    input.textContent = "This is really cool!";
+});
+
